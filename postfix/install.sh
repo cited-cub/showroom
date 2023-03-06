75bfcce3568ff0571f94227ec375a80ac58f9e61
Andreas Erlandsson <andreas.erlandsson@gmail.com>
2023-03-06 15:11:38 -0500

    Remove install.sh comments

--- postfix/install.sh
+++ postfix/install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 ip_node01=$(dig +short node01 | sort | head -n1)
 ip_node02=$(dig +short node02 | sort | head -n1)
-#echo $ip_node01 smtp1.example.com smtp1 >> /etc/hosts
-#echo $ip_node02 smtp2.example.com smtp2 >> /etc/hosts
+echo $ip_node01 smtp1.example.com smtp1 >> /etc/hosts
+echo $ip_node02 smtp2.example.com smtp2 >> /etc/hosts
 apt-get update
 DEBIAN_PRIORITY=low apt-get install -y postfix
 systemctl start postfix
